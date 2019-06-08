e891a623c2125ad52c74d8e78940aad2b4664008
Eugenia Fang <48856116+EugeniaFang789@users.noreply.github.com>
2019-06-08 12:49:35 +1000

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,23 @@
+## Loading the data
+t1 <- strptime("2006-12-16 17:24:00", format = "%Y-%m-%d %H:%M:%S")
+t2 <- strptime("2007-2-1 00:00:00", format = "%Y-%m-%d %H:%M:%S")
+t3 <- strptime("2007-2-3 00:00:00", format = "%Y-%m-%d %H:%M:%S")
+skiprownum <- as.numeric(t2-t1)*24*60
+keeprownum <- as.numeric(t3-t2)*24*60
+
+datacolname <- colnames(read.table("household_power_consumption.txt", nrow = 1, 
+                                   sep=";", header = TRUE))
+
+mydata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                     na.strings = "?", as.is = TRUE, col.names = datacolname,
+                     skip = skiprownum, nrows = keeprownum)
+
+mydata$Time <- strptime(paste(mydata$Date,mydata$Time), format="%d/%m/%Y %H:%M:%S")
+
+## Plot2
+with(mydata,plot(Time, Global_active_power, type = "l", xlab = "", 
+                 ylab = "Global Active Power (kilowatts)"))
+
+## Save to PNG
+dev.copy(png,file="plot2.png", width = 480, height = 480)
+dev.off()
